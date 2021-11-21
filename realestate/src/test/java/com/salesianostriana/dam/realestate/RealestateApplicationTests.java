837329c3ff3dc02d03fa4aafadaf1b9bfae40736
Jaime <jaimejimenezrios10@gmail.com>
2021-11-21 23:52:15 +0100

    Añadido proyecto

---
+++ realestate/src/test/java/com/salesianostriana/dam/realestate/RealestateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
